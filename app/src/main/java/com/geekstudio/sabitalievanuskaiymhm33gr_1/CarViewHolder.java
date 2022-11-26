5414840a8fbdef2a54eb3284685d472b6f5193b9
SabitalievaNuskaiym <77196324+Sabitalieva@users.noreply.github.com>
2022-11-26 18:09:50 +0600

    Initial commit

---
+++ app/src/main/java/com/geekstudio/sabitalievanuskaiymhm33gr_1/CarViewHolder.java
@@ -0,0 +1,24 @@
+package com.geekstudio.sabitalievanuskaiymhm33gr_1;
+
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CarViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView tv_car;
+
+
+    public CarViewHolder(@NonNull View itemView) {
+        super(itemView);
+        tv_car = itemView.findViewById(R.id.tv_care);
+    }
+
+    public void bind(String car) {
+        tv_car.setText(car);
+    }
+
+}
